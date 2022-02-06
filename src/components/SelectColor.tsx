eb8aa04d5e5ca081f171c30139a5773cfbbdcdd4
sangminlee98 <tkdals0975@naver.com>
2022-02-06 16:03:12 +0900

    use useContext hook

--- src/components/SelectColor.tsx
+++ src/components/SelectColor.tsx
@@ -1,33 +1,30 @@
-import React from 'react';
-import { ColorConsumer, State } from '../contexts/color';
+import React, { useContext } from 'react';
+import ColorContext from '../contexts/color';
 
 const colors = ['red','orange','yellow','green','blue','indigo','violet']
 const SelectColor = () => {
+  const {actions} = useContext(ColorContext);
   return (
     <div>
       <h2>색상을 선택하세요</h2>
-      <ColorConsumer>
-        {({actions}: State) => (
-          <div style={{display: 'flex'}}>
-            {colors.map(color => (
-              <div
-                key={color}
-                style={{
-                  background: color,
-                  width: '24px',
-                  height: '24px',
-                  cursor: 'pointer'
-                }}
-                onClick={() => actions.setColor(color)}
-                onContextMenu={e => {
-                  e.preventDefault();
-                  actions.setSubcolor(color);
-                }}
-              />
-            ))}
-          </div>
-        )}
-      </ColorConsumer>
+        <div style={{display: 'flex'}}>
+          {colors.map(color => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer'
+              }}
+              onClick={() => actions.setColor(color)}
+              onContextMenu={e => {
+                e.preventDefault();
+                actions.setSubcolor(color);
+              }}
+            />
+          ))}
+        </div>
       <hr/>
     </div>
   );
